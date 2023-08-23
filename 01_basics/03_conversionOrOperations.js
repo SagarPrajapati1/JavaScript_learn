30389b197043039ce8c3c77f6b5439248d0469ea
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2023-08-23 06:45:50 +0000

    operations

--- 01_basics/03_conversionOrOperations.js
+++ 01_basics/03_conversionOrOperations.js
@@ -1,27 +1,61 @@
-let score = null
+// let score = null
 
 //console.log(typeof score);
 //console.log(typeof (score));
 
 
-let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
-//console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0;
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+ 
 // 1 => true; 0 => false
 // "" => false
 // "anyString" => true
 
-let SomeNumber = 33
+// let SomeNumber = 33
 
-let StringNumber = String(SomeNumber);
-console.log(typeof StringNumber);
+// let StringNumber = String(SomeNumber);
+// console.log(typeof StringNumber);
+
+/********* Operations *********/
+
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**4);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " sagar"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(+true); // => 1
+// console.log(true+); // => error
+
+let value = 100
+++value
+console.log(value);
+value = 100
+value++
+console.log(value);
