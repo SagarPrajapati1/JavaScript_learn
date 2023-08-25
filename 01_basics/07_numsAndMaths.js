99b011f103f6a2a66cd07620c444ba73548d0c2f
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2023-08-25 17:09:44 +0000

    numsAndMayhs

---
+++ 01_basics/07_numsAndMaths.js
@@ -0,0 +1,41 @@
+const score = 400
+// console.log(score);
+const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString().length); // => 3
+// console.log(balance.toFixed(1)); // => 100.0
+
+const otherNumber = 123.9854
+
+// console.log(otherNumber.toPrecision(2)); // => 1.2e+2
+// console.log((otherNumber.toPrecision(4))); // => 124.0
+// console.log((otherNumber.toPrecision(3))); // => 124
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ************ Maths ************
+
+console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+
+// console.log(Math.round(4.6));
+// console.log(Math.abs(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(48));
+// console.log(Math.min(4, 5, 3, 1));
+// console.log(Math.max(4, 5, 3, 1));
+
+console.log(Math.random()); // always between 0 and 1
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor((Math.random() * 10) + 1));
+
+const min = 10
+const max = 20
+
+
+console.log(Math.floor(Math.random() * ((max-min)+1) + min));
