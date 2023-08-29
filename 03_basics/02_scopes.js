3194f13adbffc64173bc50d1d5b31adf5fb6d71a
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2023-08-29 18:51:15 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,14 @@
+const value1 = 10
+let value2 = 20
+var value3 = 30
+
+if(true) {
+    let value2 = 40
+    var value3 = 49
+
+}
+
+console.log(value1);
+console.log(value2);
+console.log(value3);
+
