ea5562569ff51e1cd4747fccce45e30da9f3e38d
Sagar Prajapati <125142824+SagarPrajapati1@users.noreply.github.com>
2023-09-01 12:01:17 +0000

    while

---
+++ 05_iteration/02_while_loop.js
@@ -0,0 +1,24 @@
+// while loop
+
+// let index = 0;
+// while(index <= 10) {
+//     console.log(`value of index is: ${index}`);
+//     index = index + 2
+// }
+
+// let myArray = ["flash", "batman", "superman"]
+
+// let i = 0;
+// while(i < myArray.length) {
+//     console.log(`value at index ${i} is ${myArray[i]}`);
+//     i = i + 1;
+// }
+
+
+// do-while loop
+
+let index = 1
+do {
+    console.log(`score is ${index}`);
+    index++;
+} while( index <= 10)
